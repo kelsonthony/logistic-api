1c6bdd95d76908fedac92ba000500eaefe27ff66
kelsonthony <k.thony@gmail.com>
2021-08-21 10:49:32 -0300

    my first setup to spring boot

---
+++ src/test/java/com/kelsonthony/logisticapi/LogisticApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelsonthony.logisticapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
